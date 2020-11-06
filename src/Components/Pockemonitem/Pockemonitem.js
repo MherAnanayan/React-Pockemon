d2b8752731a189baf1fc4b2264e040ebdc8b2c6e
Mher <52109715+MherAnanayan@users.noreply.github.com>
2020-11-06 23:10:10 +0400

    Update Pockemonitem.js

--- src/Components/Pockemonitem/Pockemonitem.js
+++ src/Components/Pockemonitem/Pockemonitem.js
@@ -14,7 +14,7 @@ const customStyles = {
         bottom: 'auto',
         marginRight: '-50%',
         transform: 'translate(-50%, -50%)',
-        width: '50%',
+        width: '33%',
         display: 'flex',
         flexFlow: 'column'
     }
